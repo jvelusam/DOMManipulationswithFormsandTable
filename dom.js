ec1ff1e9d95e15f5ab215f9f40edd17d4d050c21
Jayaveerasabari Velusamy <jayaveerasabarivelusamy@gmail.com>
2023-05-19 12:18:09 +0530

    updated the code

--- dom.js
+++ dom.js
@@ -158,7 +158,7 @@ form.appendChild(MF);
 
 
 let MF1 = document.createElement('div')
-MF1.setAttribute('class', 'row mb-4')
+MF1.setAttribute('class', 'mt-3 row mb-4')
 MF.appendChild(MF1);
 
 let MF2 = document.createElement('div')
@@ -170,25 +170,154 @@ GenderLabel.textContent = "Male:";
 GenderLabel.setAttribute('class','form-check-label d-flex  align-items-center')
 MF2.appendChild(GenderLabel);
 var GenderInput = document.createElement("input");
-GenderInput.type = "checkbox";
-GenderInput.setAttribute('class','form-check-input');
-GenderInput.setAttribute('class','form-check-input');
-GenderInput.name = "gender-0";
+GenderInput.type = "radio";
+GenderInput.setAttribute('class','form-check-label');
+GenderInput.name = "gender";
 GenderInput.id='Male'
 GenderLabel.appendChild(GenderInput);
+
+
+let GENDD = document.createElement('div')
+GENDD.setAttribute('class', 'formbuilder-radio-group form-group field-gender')
+form.appendChild(GENDD);
+
+
+let GENDD1 = document.createElement('div')
+GENDD1.setAttribute('class', 'radio-group')
+GENDD.appendChild(GENDD1);
+
+let GENDD2 = document.createElement('div')
+GENDD2.setAttribute('class', 'formbuilder-radio')
+GENDD1.appendChild(GENDD2);
+
 
 var Gender1 = document.createElement("label");
 Gender1.setAttribute('class','form-check-label d-flex  align-items-center')
 Gender1.textContent = "Female:";
-MF2.appendChild(Gender1);
+GENDD2.appendChild(Gender1);
+
 var GenderInput1 = document.createElement("input");
-GenderInput1.type = "checkbox";
-GenderInput1.setAttribute('class','form-check-input');
-GenderInput1.name = "gender-1";
-GenderInput.id='Female'
+GenderInput1.type = "radio";
+GenderInput1.setAttribute('class','form-check-label');
+GenderInput1.name = "gender";
+GenderInput1.id='Female'
 Gender1.appendChild(GenderInput1);
 
 
+
+{/* <div class="formbuilder-checkbox-group form-group field-food">
+    <label for="food" class="formbuilder-checkbox-group-label">Food Selections<span class="formbuilder-required">*</span></label>
+    <div class="checkbox-group">
+        <div class="formbuilder-checkbox">
+            <input class="form-check-label" name="food[]" access="false" id="food-0" aria-required="true" value="Chapati" type="checkbox" checked="checked">
+            <label for="food-0">Chapati</label>
+        </div>
+        <div class="formbuilder-checkbox">
+            <input class="form-check-label" name="food[]" access="false" id="food-1" aria-required="true" value="Poori" type="checkbox" checked="checked">
+            <label for="food-1">Poori</label>
+        </div>
+        <div class="formbuilder-checkbox">
+            <input class="form-check-label" name="food[]" access="false" id="food-2" aria-required="true" value="Mutton" type="checkbox" checked="checked">
+            <label for="food-2">Mutton</label>
+        </div>
+        <div class="formbuilder-checkbox">
+            <input class="form-check-label" name="food[]" access="false" id="food-3" aria-required="true" value="Chicken" type="checkbox" checked="checked">
+            <label for="food-3">Chicken</label>
+        </div>
+        <div class="formbuilder-checkbox">
+            <input class="form-check-label" name="food[]" access="false" id="food-4" aria-required="true" value="Veg" type="checkbox" checked="checked">
+            <label for="food-4">Veg</label>
+        </div>
+        <div class="formbuilder-checkbox">
+            <input class="form-check-label" name="food[]" access="false" id="food-5" aria-required="true" value="Paneer" type="checkbox" checked="checked">
+            <label for="food-5">Paneer</label>
+        </div>
+    </div>
+</div> */}
+
+let FoodNonVegMeal = document.createElement('div')
+FoodNonVegMeal.setAttribute('class', 'formbuilder-checkbox-group form-group field-food')
+form.appendChild(FoodNonVegMeal);
+
+let FoodNonVegM1 = document.createElement('div')
+FoodNonVegM1.setAttribute('class', 'checkbox-group')
+FoodNonVegMeal.appendChild(FoodNonVegM1);
+
+let FoodNonVegM2 = document.createElement('div')
+FoodNonVegM2.setAttribute('class', 'formbuilder-checkbox')
+FoodNonVegM1.appendChild(FoodNonVegM2);
+
+
+var FoodNVegLabC1 = document.createElement("label");
+FoodNVegLabC1.textContent = "Non Veg Meal:";
+FoodNVegLabC1.setAttribute('for','food-0');
+FoodNonVegM2.appendChild(FoodNVegLabC1);
+
+
+var FoodNVegCInput1 = document.createElement("input");
+FoodNVegCInput1.type = "checkbox";
+FoodNVegCInput1.setAttribute('class','form-check-label');
+FoodNVegCInput1.name = "food[]";
+FoodNVegCInput1.id='Food-0'
+FoodNVegLabC1.appendChild(FoodNVegCInput1);
+
+
+
+
+let FoodM = document.createElement('div')
+FoodM.setAttribute('class', 'formbuilder-checkbox-group form-group field-food')
+form.appendChild(FoodM);
+
+let FooddM1 = document.createElement('div')
+FooddM1.setAttribute('class', 'checkbox-group')
+FoodM.appendChild(FooddM1);
+
+let FooddM2 = document.createElement('div')
+FooddM2.setAttribute('class', 'formbuilder-checkbox')
+FooddM1.appendChild(FooddM2);
+
+
+var FoodMLabC1 = document.createElement("label");
+FoodMLabC1.textContent = "Mutton:";
+FoodMLabC1.setAttribute('for','food-1');
+FoodNonVegM2.appendChild(FoodMLabC1);
+
+
+var FoodMCInput1 = document.createElement("input");
+FoodMCInput1.type = "checkbox";
+FoodMCInput1.setAttribute('class','form-check-label');
+FoodMCInput1.name = "food[]";
+FoodMCInput1.id='Food-1'
+FoodMLabC1.appendChild(FoodMCInput1);
+
+
+
+
+let FoodVeg = document.createElement('div')
+FoodVeg.setAttribute('class', 'formbuilder-checkbox-group form-group field-food')
+form.appendChild(FoodVeg);
+
+let FoodVeg1 = document.createElement('div')
+FoodVeg1.setAttribute('class', 'checkbox-group')
+FoodVeg.appendChild(FoodVeg1);
+
+let FoodVeg2 = document.createElement('div')
+FoodVeg2.setAttribute('class', 'formbuilder-checkbox')
+FoodVeg1.appendChild(FoodVeg2);
+
+
+var FoodVegLabC1 = document.createElement("label");
+FoodVegLabC1.textContent = "Veg Meal:";
+FoodVegLabC1.setAttribute('for','food-2');
+FoodNonVegM2.appendChild(FoodVegLabC1);
+
+
+var FoodvegCInput1 = document.createElement("input");
+FoodvegCInput1.type = "checkbox";
+FoodvegCInput1.setAttribute('class','form-check-label');
+FoodvegCInput1.name = "food[]";
+FoodvegCInput1.id='Food-2'
+FoodVegLabC1.appendChild(FoodvegCInput1);
 
 
 
@@ -207,17 +336,44 @@ submitButton.setAttribute('class','btn btn-primary w-100')
 submitted.appendChild(submitButton);
 
 
-
+let formValues = 
+{
+    FristName: "F Name ",
+    LastName: "L Name ",
+    Addresse: "Addresse",
+    Pincode: "1",
+    Gender: "M",
+    Food: [],
+    State: "TN",
+    Country: "IND",
+}
 
 submitButton.addEventListener("click", function (event) 
 {
   
+
     event.preventDefault();
+
+    let GEX = document.querySelector('input[name="gender"]:checked').id
+    console.log(GEX);
+
+
+    let Fodds = document.querySelectorAll('input[name="food"]:checked')
+        console.log(Fodds);
+
+        Fodds.forEach(element => 
+        {
+            console.log(element.id);
+            formValues.Food.push(element.id);
+        });
+
+
     var FRISTNAME = FNInput.value;
     var LASTNAMES = LNInput.value;
     var ADDRESSESS = ADDInput.value;
     var ZIPCODE = ZIPInput.value;
-    var GEND = GenderInput.id;
+    var GEND = GEX;
+    var Foodie = formValues.Food;
     var table = document.getElementById("dataTable");
     var row = table.insertRow();
     var nameCell = row.insertCell();
@@ -225,11 +381,13 @@ submitButton.addEventListener("click", function (event)
     var addrCell = row.insertCell();
     var ZipCell = row.insertCell();
     var GEN = row.insertCell();
+    var Fod = row.insertCell();
     nameCell.textContent = FRISTNAME;
     emailCell.textContent = LASTNAMES;
     addrCell.textContent = ADDRESSESS;
     ZipCell.textContent = ZIPCODE;
     GEN.textContent = GEND;
+    Fod.textContent = Foodie;
   
     form.reset();
 });
@@ -248,6 +406,9 @@ var zipp = headerRow.insertCell();
 zipp.textContent = "ZIPCODE";
 var gen = headerRow.insertCell();
 gen.textContent = "Gender";
+var Fodd = headerRow.insertCell();
+Fodd.textContent = "Food Selections";
+
 
 // add form and table to document
 document.body.appendChild(form);
@@ -263,5 +424,11 @@ form.appendChild(GenderLabel);
 form.appendChild(GenderInput);
 form.appendChild(Gender1);
 form.appendChild(GenderInput1);
+form.appendChild(FoodNVegLabC1);
+form.appendChild(FoodNVegCInput1);
+form.appendChild(FoodMLabC1);
+form.appendChild(FoodMCInput1);
+form.appendChild(FoodVegLabC1);
+form.appendChild(FoodvegCInput1);
 form.appendChild(submitButton);
 document.body.appendChild(table);
